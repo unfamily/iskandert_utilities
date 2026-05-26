cb05906bc56b23e2b1b46f71d1652573fb60d93a
unfamily <mirko9876dev@gmail.com>
2026-05-27 01:32:08 +0200

    .

--- src/main/java/net/unfamily/iskautils/item/custom/ProductionModuleItem.java
+++ src/main/java/net/unfamily/iskautils/item/custom/ProductionModuleItem.java
@@ -1,5 +1,7 @@
 package net.unfamily.iskautils.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -12,7 +14,7 @@ import java.util.List;
 
 /**
  * Production Module: Pattern Crafter only (1.2.0.0.0+).
- * Tooltip is shown only when a compatible Pattern Crafter version is loaded.
+ * Tooltip is shown on Shift when a compatible Pattern Crafter version is loaded.
  */
 public class ProductionModuleItem extends Item {
 
@@ -25,8 +27,15 @@ public class ProductionModuleItem extends Item {
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flag) {
         super.appendHoverText(stack, context, tooltip, flag);
 
-        if (PatternCrafterTooltipHelper.supportsProductionModule()) {
+        if (!PatternCrafterTooltipHelper.supportsProductionModule()) {
+            return;
+        }
+
+        if (Screen.hasShiftDown()) {
             PatternCrafterTooltipHelper.addProductionModuleTooltip(tooltip);
+        } else {
+            tooltip.add(Component.translatable("tooltip.iska_utils.fan_module.press_shift")
+                    .withStyle(ChatFormatting.GRAY));
         }
     }
 }
